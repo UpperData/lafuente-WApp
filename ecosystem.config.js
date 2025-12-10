d84518afd9b639e296cc3218451c71ec62ce6451
ANGEL DIAZ <upper.venezuela@gmail.com>
2025-12-10 16:48:48 -0400

    pm2 ecosistem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ export const apps = [
     {
         name: 'front.lafuente',
         script: 'npm',
-        args: 'run dev',
+        args: 'run start',
         env: {
             VITE_OPEN: 'false',
             VITE_PORT: '5170',
